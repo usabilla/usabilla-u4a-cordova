e0d0b707466ac911c27ad13519beefb87a98635e
Lorenzo Marchiori <lorenzo@usabilla.com>
2018-07-10 16:24:49 +0200

    fix(android): update example app

--- src/android/UsabillaCordova.java
+++ src/android/UsabillaCordova.java
@@ -1,118 +1,128 @@
 package com.usabilla;
 
-import org.apache.cordova.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v4.app.FragmentActivity;
 
 import com.usabilla.sdk.ubform.Usabilla;
-import com.usabilla.sdk.ubform.UsabillaReadyCallback;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import static com.usabilla.Constant.APP_ID;
+import static com.usabilla.Constant.EVENT_NAME;
+import static com.usabilla.Constant.FORM_ID;
+import static com.usabilla.Constant.SCREENSHOT_NAME;
+
 public class UsabillaCordova extends CordovaPlugin {
+
     private CallbackContext callbackContext;
-    private Usabilla usabilla;
-    private String formId;
     private String appId;
-
-    public HashMap<String, Object> parseOptions(JSONObject dataObj) throws JSONException {
-        if (dataObj == null) {
-            new HashMap<String, Object>();
-        }
-
-        HashMap<String, Object> customVars = new HashMap<String, Object>();
-        for (Iterator<String> it = dataObj.keys(); it.hasNext(); ) {
-            String key = it.next();
-            Object value = dataObj.get(key);
-
-            if ("FORM_ID".equals(key)) {
-                this.formId = (String)value;
-            } else if (("APP_ID").equals(key)) {
-                this.appId = (String)value;
-            } else {
-                customVars.put(key, value);
-            }
-        }
-        return customVars;
-    }
-
+    private String formId;
+    private Usabilla usabilla;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
-
-        if (action.equals("loadFeedbackForm")) {
-            this.parseOptions((JSONObject) data.get(0));
-            Intent intent = new Intent(cordova.getActivity(), UsabillaActivity.class);
-            intent.putExtra("FORM_ID", this.formId);
-
-            if (this.cordova != null) {
-                this.cordova.startActivityForResult( this, intent, 0);
-            }
-            return true;
-
-        } else if (action.equals("initialize")) {
-            HashMap<String, Object> customVars = this.parseOptions((JSONObject) data.get(0));
-            this.initialize(customVars);
-            return true;
-        } else if (action.equals("resetCampaignData")) {
-            this.resetCampaignData();
-            return true;
-        } else if (action.equals("sendEvent")) {
-            JSONObject dataObj = (JSONObject) data.get(0);
-            this.sendEvent((String)dataObj.get("EVENT_NAME"));
-            return true;
-        } else {
-            return false;
+        switch (action) {
+            case "initialize":
+                final HashMap<String, Object> customVars = parseOptions((JSONObject) data.get(0));
+                initialize(customVars, appId);
+                return true;
+            case "loadFeedbackForm":
+                loadForm((JSONObject) data.get(0), false);
+                return true;
+            case "loadFeedbackFormWithCurrentViewScreenshot":
+                loadForm((JSONObject) data.get(0), true);
+                return true;
+            case "resetCampaignData":
+                resetCampaignData();
+                return true;
+            case "sendEvent":
+                final JSONObject dataObj = (JSONObject) data.get(0);
+                sendEvent((String) dataObj.get(EVENT_NAME));
+                return true;
+            default:
+                return false;
         }
-    }
-
-    public void initialize(HashMap<String, Object> customVars) {
-        usabilla = Usabilla.Companion.getInstance(cordova.getActivity());
-        usabilla.initialize(this.cordova.getActivity(), this.appId, new UsabillaReadyCallback() {
-            @Override
-            public void onUsabillaInitialized() {
-                usabilla.updateFragmentManager(((FragmentActivity)UsabillaCordova.this.cordova.getActivity()).getSupportFragmentManager());
-                UsabillaCordova.this.onActivityResult(0, Activity.RESULT_OK, null);
-            }
-        });
-        usabilla.setCustomVariables(customVars);
-    }
-
-    public void sendEvent(String eventName) {
-        usabilla.sendEvent(this.cordova.getActivity(), eventName);
-        this.onActivityResult(0, Activity.RESULT_OK, null);
-    }
-
-    public void resetCampaignData() {
-        usabilla.resetCampaignData(this.cordova.getActivity(), new UsabillaReadyCallback() {
-            @Override
-            public void onUsabillaInitialized() {
-                UsabillaCordova.this.onActivityResult(0, Activity.RESULT_OK, null);
-            }
-        });
-
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == 5) {
-            this.callbackContext.error("GENERAL_ERROR");
+            callbackContext.error("GENERAL_ERROR");
         } else {
             try {
                 JSONObject result = new JSONObject();
                 result.put("completed", resultCode != Activity.RESULT_CANCELED);
-                this.callbackContext.success(result);
+                callbackContext.success(result);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            this.callbackContext.success();
+            callbackContext.success();
         }
+    }
+
+    private void initialize(HashMap<String, Object> customVars, String appId) {
+        usabilla = Usabilla.Companion.getInstance(cordova.getActivity());
+        usabilla.initialize(cordova.getActivity(), appId, () -> {
+            usabilla.updateFragmentManager(((FragmentActivity) cordova.getActivity()).getSupportFragmentManager());
+            UsabillaCordova.this.onActivityResult(0, Activity.RESULT_OK, null);
+        });
+        usabilla.setCustomVariables(customVars);
+    }
+
+    private void loadForm(JSONObject data, boolean withScreenshot) throws JSONException {
+        final Intent intent = new Intent(cordova.getActivity(), UsabillaActivity.class);
+        parseOptions(data);
+        if (formId != null) {
+            intent.putExtra(FORM_ID, formId);
+        }
+        if (withScreenshot) {
+            final Bitmap screenshot = usabilla.takeScreenshot(cordova.getActivity());
+            if (screenshot != null) {
+                try (FileOutputStream out = cordova.getContext().openFileOutput(SCREENSHOT_NAME, Context.MODE_PRIVATE)) {
+                    screenshot.compress(Bitmap.CompressFormat.PNG, 100, out);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        cordova.startActivityForResult(this, intent, 0);
+    }
+
+    private void resetCampaignData() {
+        usabilla.resetCampaignData(cordova.getActivity(), () -> UsabillaCordova.this.onActivityResult(0, Activity.RESULT_OK, null));
+    }
+
+    private void sendEvent(String eventName) {
+        usabilla.sendEvent(cordova.getActivity(), eventName);
+        this.onActivityResult(0, Activity.RESULT_OK, null);
+    }
+
+    private HashMap<String, Object> parseOptions(JSONObject dataObj) throws JSONException {
+        final HashMap<String, Object> customVars = new HashMap<>();
+        if (dataObj != null) {
+            for (Iterator<String> it = dataObj.keys(); it.hasNext(); ) {
+                final String key = it.next();
+                final Object value = dataObj.get(key);
+                if (FORM_ID.equals(key)) {
+                    formId = (String) value;
+                } else if ((APP_ID).equals(key)) {
+                    appId = (String) value;
+                } else {
+                    customVars.put(key, value);
+                }
+            }
+        }
+        return customVars;
     }
 }
