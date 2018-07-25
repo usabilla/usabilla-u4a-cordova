9150156022ab0b600800b7f5c5529bb97d6777f7
Lorenzo Marchiori <lorenzo@usabilla.com>
2018-07-25 15:21:26 +0200

    fix(android): remove Constant class

--- src/android/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.usabilla;
-
-class Constant {
-    static final String APP_ID = "APP_ID";
-    static final String EVENT_NAME = "EVENT_NAME";
-    static final String FORM_ID = "FORM_ID";
-    static final String SCREENSHOT_NAME = "screenshot";
-}
